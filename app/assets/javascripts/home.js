0c669f445a97f9cbca9d92dd9f73b849e148a586
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2016-02-15 14:37:08 -0600

    navigation bar and toggle menu

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,29 @@
+// Example code from here: http://www.w3schools.com/bootstrap/bootstrap_ref_js_carousel.asp
+// for bootstrap's carouse methods. 
+//
+// $(document).ready(function(){
+//     // Activate Carousel
+//     $("#myCarousel").carousel({interval: 500});
+    
+//     // Enable Carousel Indicators
+//     $(".item1").click(function(){
+//         $("#myCarousel").carousel(0);
+//     });
+//     $(".item2").click(function(){
+//         $("#myCarousel").carousel(1);
+//     });
+//     $(".item3").click(function(){
+//         $("#myCarousel").carousel(2);
+//     });
+//     $(".item4").click(function(){
+//         $("#myCarousel").carousel(3);
+//     });
+    
+//     // Enable Carousel Controls
+//     $(".left").click(function(){
+//         $("#myCarousel").carousel("prev");
+//     });
+//     $(".right").click(function(){
+//         $("#myCarousel").carousel("next");
+//     });
+// });
