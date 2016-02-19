928021ca45980cbd17689a77d86dd339ec147c00
Diego Diaz de Berenguer <d.diazdeberenguer@gmail.com>
2016-02-19 01:27:51 -0600

    add the full-screen cover image for carousel

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,49 @@
+$(document).ready(function() {
+	"use strict";
+
+	var topoffset = 50;
+
+	//Activate Scrollspy
+	$('body').scrollspy ({
+		target: 'header .navbar',
+		offset: topoffset
+	});
+
+
+	// Add class to navbar when user scrolls down.
+	var scroll_start = 0;
+	var startchange = $('#top');
+	var offset = startchange.offset();
+
+	if (startchange.length){
+		$(document).scroll(function() {
+			scroll_start = $(this).scrollTop();
+			if(scroll_start > (offset.top+300) ) {
+				$("header .navbar").addClass("past-scroll")
+			} else {
+				$("header .navbar").removeClass("past-scroll")
+			}
+
+		});
+	}
+
+	var screenHeight = $(window).height();
+
+	$('.full-height').css('height', screenHeight);
+
+	$(window).resize(function() {
+		screenHeight = $(window).height();
+		$('.full-height').css('height', screenHeight);
+	});
+
+	$('#topside .item img').each(function() {
+		var imgSrc = $(this).attr('src');
+		$(this).parent().css({'background-image': 'url('+imgSrc+')'});
+		$(this).remove();
+	});
+
+});
+
 // Example code from here: http://www.w3schools.com/bootstrap/bootstrap_ref_js_carousel.asp
 // for bootstrap's carouse methods. 
 //
